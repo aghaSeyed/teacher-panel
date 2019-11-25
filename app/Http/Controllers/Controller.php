06d763c8cc5a3a479ddb688dc1c15081d469aa02
sajad-sadra <mss.sadra137812133@gmail.com>
2019-11-25 21:02:43 +0330

    a comment added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+// Fuck You
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
